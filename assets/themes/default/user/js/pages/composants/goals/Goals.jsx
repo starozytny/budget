de3adaba0e29dd45cd43ae8622c884d51a9ba978
Starozytny <starozytny@hotmail.fr>
2020-11-14 17:58:43 +0100

    get more infos for goals

--- assets/themes/default/user/js/pages/composants/goals/Goals.jsx
+++ assets/themes/default/user/js/pages/composants/goals/Goals.jsx
@@ -17,6 +17,23 @@ function setCurrency(price){
     return new Intl.NumberFormat("de-DE", {style: "currency", currency: "EUR"}).format(price);
 }
 
+function getMonthString(expr){
+    switch (expr) {
+        case 0: return "Janvier"
+        case 1: return "Février"
+        case 2: return "Mars"
+        case 3: return "Avril"
+        case 4: return "Mai"
+        case 5: return "Juin"
+        case 6: return "Juillet"
+        case 7: return "Août"
+        case 8: return "Septembre"
+        case 9: return "Octobre"
+        case 10: return "Novembre"
+        default: return "Décembre"
+      }
+}
+
 export class Goals extends Component {
     constructor (props){
         super ()
@@ -52,8 +69,26 @@ export class Goals extends Component {
     render () {
         const {goals} = this.state
 
+        let now = new Date();
         let items = goals.map(elem => {
-            return <div className="card-1">
+
+            let totNow = 0, prevYear = 0, prevMonth = 0, finalYear = 0, finalMonth = 0;
+            elem.economy.forEach(eco => {
+                let y = eco.budget.year
+                let m = eco.budget.month
+                if( (y < now.getFullYear()) || (y == now.getFullYear() && m <= now.getMonth() + 1) ){
+                    totNow += eco.price
+                }
+
+                if((prevYear < y) || (prevYear == y && prevMonth < m) || (prevYear > y)){
+                    finalYear = y; finalMonth = m;
+                }
+
+                prevYear = y
+                prevMonth = m
+            })
+
+            return <div className="card-1" key={elem.id}>
                 {/* <div class="card-1-drawing">
                     <div class="image">
                         <img src="{{ asset(path_images ~ "drawing.jpg") }}" alt="illustration">
@@ -63,6 +98,10 @@ export class Goals extends Component {
                     <div className="title">{elem.name}</div>
                 </div>
                 <div className="card-1-body">
+                    <div>Total atteint au mois de {getMonthString(now.getMonth()).toLowerCase()}</div>
+                    <div>{setCurrency(totNow)} / {setCurrency(elem.total)}</div>
+
+                    <div>Total atteint en {getMonthString(finalMonth-1).toLowerCase()} {finalYear}</div>
                     <div>{setCurrency(elem.fill)} / {setCurrency(elem.total)}</div>
                 </div>
                 <div className="card-1-footer">
