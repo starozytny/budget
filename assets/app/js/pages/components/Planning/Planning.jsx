99062bd97c282367ee3292869ce40fe499539647
Starozytny <starozytny@hotmail.fr>
2021-10-17 10:14:20 +0200

    months planning

--- assets/app/js/pages/components/Planning/Planning.jsx
+++ assets/app/js/pages/components/Planning/Planning.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 
+import {Months} from "@dashboardComponents/Tools/Days";
+
 export class Planning extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            data: [ {day: 1}, {day: 2}, {day: 3}, {day: 4}, {day: 5} ],
+            dayActive: 1
+        }
+
+        this.handleSelectDay = this.handleSelectDay.bind(this);
+    }
+
+    handleSelectDay = (dayActive, atLeastOne) => {
+        if(atLeastOne) {
+            this.setState({ dayActive })
         }
     }
 
     render () {
+        const { data, dayActive } = this.state;
 
-        return <div>Hello world</div>
+        return <>
+            <Months data={data} dayActive={dayActive} onSelectDay={this.handleSelectDay}/>
+        </>
     }
 }
--- assets/dashboard/js/components/Tools/Days.jsx
+++ assets/dashboard/js/components/Tools/Days.jsx
@@ -38,3 +38,48 @@ export class Days extends Component {
         </>
     }
 }
+
+export class Months extends Component {
+    render () {
+        const { data, dayActive, onSelectDay, useShortName = false } = this.props
+
+        let days = [
+            { id: 1, name: 'Janvier',       shortName: 'Jan' },
+            { id: 2, name: 'Février',       shortName: 'Dev' },
+            { id: 3, name: 'Mars',          shortName: 'Mar' },
+            { id: 4, name: 'Avril',         shortName: 'Avr' },
+            { id: 5, name: 'Mai',           shortName: 'Mai' },
+            { id: 6, name: 'Juin',          shortName: 'Juin' },
+            { id: 7, name: 'Juillet',       shortName: 'Juil' },
+            { id: 8, name: 'Août',          shortName: 'Aou' },
+            { id: 9, name: 'Septembre',     shortName: 'Sep' },
+            { id: 10, name: 'Octobre',       shortName: 'Oct' },
+            { id: 11, name: 'Novembre',      shortName: 'Nov' },
+            { id: 12, name: 'Décembre',      shortName: 'Déc' },
+        ];
+
+        let items = days.map(elem => {
+
+            let atLeastOne = false;
+            if(data){
+                data.forEach(el => {
+                    if(el.day === elem.id || (el.slot && el.slot.day === elem.id)){
+                        atLeastOne = true;
+                    }
+                })
+            }
+
+            return <div className={"day" + (elem.id === dayActive ? " active" : "") + (atLeastOne ? "" : " disabled")}
+                        onClick={() => onSelectDay(elem.id, atLeastOne)}
+                        key={elem.id}>
+                {useShortName ? elem.shortName : elem.name}
+            </div>
+        })
+
+        return <>
+            <div className="days">
+                {items}
+            </div>
+        </>
+    }
+}
